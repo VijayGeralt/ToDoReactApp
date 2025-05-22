77e5ce5c4d53430dcccc770ed6f50aa8dfc92122
VIJAY SHARMA <www.710vijaysharma@gmail.com>
2025-05-23 04:16:22 +0530

    Bug fix for initial code

--- src/Components/Todo.jsx
+++ src/Components/Todo.jsx
@@ -8,10 +8,10 @@ let count = 0;
 const Todo = () => {
 
     const [todos, setTodos] = useState([]);
-    const[inputref] = useRef(null)
+    const inputref = useRef(null);
 
     const add = () =>{
-        setTodos([...todos, {no:count++, text:inputref.current.value, display: "" }]);
+        setTodos([...todos, {no:count++,text:inputref.current.value,display:"" }]);
 
         // refresh the value
         inputref.current.value = "";
@@ -20,17 +20,18 @@ const Todo = () => {
     useEffect(
         () =>{
             console.log(todos);
-        }, [todos])
+        },[todos]
+    )
 
   return (
     <div className='todo'>
         <div className="todo-header">To-Do List</div>
         <div className="todo-add">
             <input ref={inputref} type="text"  placeholder='Add your task' className='todo-input' />
-            <div onClick={()=>{add}} className="todo-add-btn">ADD</div>
+            <div onClick={()=>{add()}} className="todo-add-btn">ADD</div>
         </div>
         <div className="todo-list">
-
+            
         </div>
     </div>
   )
