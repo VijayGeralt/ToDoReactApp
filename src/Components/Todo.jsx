21fbe985c96ae70bebb4cabfd4ff4e7c0da130f6
VIJAY SHARMA <www.710vijaysharma@gmail.com>
2025-05-26 02:36:43 +0530

    Final code commit

--- src/Components/Todo.jsx
+++ src/Components/Todo.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useRef, useState } from 'react';
 import './CSS/Todo.css'
+import Todoitems from './Todoitems';
 
 
 
@@ -10,16 +11,36 @@ const Todo = () => {
     const [todos, setTodos] = useState([]);
     const inputref = useRef(null);
 
-    const add = () =>{
+    const AddTodo = () =>{
         setTodos([...todos, {no:count++,text:inputref.current.value,display:"" }]);
 
         // refresh the value
         inputref.current.value = "";
+
+        // Set count variable to localstorage
+        localStorage.setItem('todos_count', count);
     }
 
+    // This useEffect hook gets called when the page reloads
     useEffect(
-        () =>{
-            console.log(todos);
+        () => {
+            // Sets the todos state after getting the "todos" item from local storage
+            setTodos(JSON.parse(localStorage.getItem("todos")));
+            // Sets the count variable from the "todos_count" item in local storage
+            count = localStorage.getItem('todos_count');
+        }, []
+    )
+
+    // This useEffect hook gets called when "todos" state changes
+    // Save todos to localstorage when the component (todos) updates
+    useEffect(
+        () => {
+            setTimeout(
+                () => {
+                    console.log(todos);
+                    localStorage.setItem("todos", JSON.stringify(todos));
+                }, 100
+            );
         },[todos]
     )
 
@@ -27,11 +48,13 @@ const Todo = () => {
     <div className='todo'>
         <div className="todo-header">To-Do List</div>
         <div className="todo-add">
-            <input ref={inputref} type="text"  placeholder='Add your task' className='todo-input' />
-            <div onClick={()=>{add()}} className="todo-add-btn">ADD</div>
+            <input ref={inputref} type="text" placeholder='Add your task' className='todo-input' />
+            <div onClick={()=>{AddTodo()}} className="todo-add-btn">ADD</div>
         </div>
         <div className="todo-list">
-            
+            {todos.map((item, index)=>{
+                return <Todoitems key={index} setTodos={setTodos} no={item.no} display ={item.display} text={item.text} />
+            })}
         </div>
     </div>
   )
--- src/Components/Todoitems.jsx
+++ src/Components/Todoitems.jsx
@@ -1,9 +1,51 @@
 import './CSS/Todoitems.css'
+import tick from './Assests/tick.png'
+import cross from './Assests/cross.png'
+import not_tick from './Assests/not_tick.png'
 
-const Todoitems = () => {
+const Todoitems = ({no, display, text, setTodos}) => {
+
+  const DeleteTodo = (no) =>{
+    // Gets the todos data
+    let todosData = JSON.parse(localStorage.getItem('todos'));
+    // Filters only the todosData whose no is not equal to the no of the item to be deleted
+    todosData = todosData.filter((todo) => todo.no !== no);
+
+    // Now we update the todosData and afterwards the useEffect will eventually update the todos in localStorage
+    setTodos(todosData); 
+  }
+
+  const Toggle = () => {
+    let todosData = JSON.parse(localStorage.getItem('todos'));
+
+    for (let i = 0; i < todosData.length; i++)
+    {
+      if(todosData[i].no === no) {
+        if(todosData[i].display === "")
+        {
+          todosData[i].display = 'line-through';
+        }
+        else {
+          todosData[i].display = '';
+        }
+
+        break;
+      }
+    }
+
+    setTodos(todosData);
+  }
+
   return (
-    <div>
-
+    <div className='todoitems'>
+      {/* <div className="todoitems-container" onClick={()=>Toggle()}></div> */}
+      <div className={`todoitems-container ${display}`} onClick={()=>Toggle()}>
+        {display === "" ? <img src={not_tick} alt="" /> : <img src={tick} alt="" />}
+        {/* <img src={not_tick} alt="" />
+        <img src={tick} alt="" /> */}
+        <div className="todoitems-text">{text}</div>
+      </div>
+      <img className='todoitems-cross-icon' src={cross} onClick={()=>{DeleteTodo(no)}} alt="" />
     </div>
   )
 }
